3b321e9d67ecbbad4144d401cf4d201fc05419fe
Karl-Eric Jolicoeur <karlaujo168@gmail.com>
2024-04-03 12:21:54 -0400

    indentation

--- lib/views/screens/game_screen/game_screen.dart
+++ lib/views/screens/game_screen/game_screen.dart
@@ -35,171 +35,171 @@ class _GameScreenState extends State<GameScreen> {
     return ChangeNotifierProvider.value(
       value: _viewModel,
       child: Consumer<GameViewModel>(
-      builder: (context, viewModel, child) {
-        return Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFF6B8E23),
-                  Color(0xFFFFFFFF),
-                ],
+        builder: (context, viewModel, child) {
+          return Scaffold(
+            body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF6B8E23),
+                    Color(0xFFFFFFFF),
+                  ],
+                ),
               ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(height: 40),
-                Text(
-                  'WordLink',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.pacifico(
-                    fontSize: 48,
-                    color: Colors.white,
-                    shadows: [
-                      const Shadow(
-                        color: Colors.black,
-                        offset: Offset(2, 2),
-                        blurRadius: 4,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 40),
+                  Text(
+                    'WordLink',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.pacifico(
+                      fontSize: 48,
+                      color: Colors.white,
+                      shadows: [
+                        const Shadow(
+                          color: Colors.black,
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DictionarySelectorScreen(
+                                onDictionarySelected: (dictionaryUrl) {
+                                  _viewModel.updateDictionaryUrl(dictionaryUrl);
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: const StadiumBorder(), backgroundColor: const Color(0xFF9CCC65),
+                          elevation: 4,
+                        ),
+                        child:  Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                          child: Text(AppLocalizations.of(context)!.dictionary),
+                        ),
+                      ),
+                      ElevatedButton(
+                        // GameScreen 
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LanguageSelectorScreen(
+                                onLanguageSelected: (language) {
+                                  _viewModel.setLanguage(language);
+                                  print('Selected language: $language');
+                                  
+                                }, 
+                                viewModel: _viewModel,
+                              ),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: const StadiumBorder(), backgroundColor: const Color(0xFF9CCC65),
+                          elevation: 4,
+                        ),
+                        child:  Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                          child: Text(AppLocalizations.of(context)!.language),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle start game
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Provider<TimerService>(
+                                  create: (_) =>TimerService(),
+                                  child: StartGameScreen(language: widget.language, selectedLevel: _selectedLevel, viewModel: _viewModel)
+                              ),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: const StadiumBorder(), backgroundColor: const Color(0xFF9CCC65),
+                          elevation: 4,
+                        ),
+                        child:  Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                          child: Text(AppLocalizations.of(context)!.startGame),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DictionarySelectorScreen(
-                              onDictionarySelected: (dictionaryUrl) {
-                                _viewModel.updateDictionaryUrl(dictionaryUrl);
-                              },
-                            ),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(), backgroundColor: const Color(0xFF9CCC65),
-                        elevation: 4,
-                      ),
-                      child:  Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                        child: Text(AppLocalizations.of(context)!.dictionary),
-                      ),
+                  const SizedBox(height: 40),
+                  Text(
+                    AppLocalizations.of(context)!.selectLevel,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.roboto(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                    ElevatedButton(
-                      // GameScreen 
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LanguageSelectorScreen(
-                              onLanguageSelected: (language) {
-                                _viewModel.setLanguage(language);
-                                print('Selected language: $language');
-                                
-                              }, 
-                              viewModel: _viewModel,
-                            ),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(), backgroundColor: const Color(0xFF9CCC65),
-                        elevation: 4,
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Radio(
+                        value: 0,
+                        groupValue: _selectedLevel,
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedLevel = value as int;
+                          });
+                        },
+                        activeColor: const Color(0xFF9CCC65),
                       ),
-                      child:  Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                        child: Text(AppLocalizations.of(context)!.language),
+                      Text(AppLocalizations.of(context)!.easy),
+                      const SizedBox(width: 20),
+                      Radio(
+                        value: 1,
+                        groupValue: _selectedLevel,
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedLevel = value as int;
+                          });
+                        },
+                        activeColor: const Color(0xFF9CCC65),
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle start game
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Provider<TimerService>(
-                                create: (_) =>TimerService(),
-                                child: StartGameScreen(language: widget.language, selectedLevel: _selectedLevel, viewModel: _viewModel)
-                            ),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(), backgroundColor: const Color(0xFF9CCC65),
-                        elevation: 4,
+                      Text(AppLocalizations.of(context)!.medium),
+                      const SizedBox(width: 20),
+                      Radio(
+                        value: 2,
+                        groupValue: _selectedLevel,
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedLevel = value as int;
+                          });
+                        },
+                        activeColor: const Color(0xFF9CCC65),
                       ),
-                      child:  Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                        child: Text(AppLocalizations.of(context)!.startGame),
-                      ),
+                      Text(AppLocalizations.of(context)!.hard),
+                      ],
                     ),
                   ],
                 ),
-                const SizedBox(height: 40),
-                Text(
-                  AppLocalizations.of(context)!.selectLevel,
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.roboto(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Radio(
-                      value: 0,
-                      groupValue: _selectedLevel,
-                      onChanged: (value) {
-                        setState(() {
-                          _selectedLevel = value as int;
-                        });
-                      },
-                      activeColor: const Color(0xFF9CCC65),
-                    ),
-                    Text(AppLocalizations.of(context)!.easy),
-                    const SizedBox(width: 20),
-                    Radio(
-                      value: 1,
-                      groupValue: _selectedLevel,
-                      onChanged: (value) {
-                        setState(() {
-                          _selectedLevel = value as int;
-                        });
-                      },
-                      activeColor: const Color(0xFF9CCC65),
-                    ),
-                    Text(AppLocalizations.of(context)!.medium),
-                    const SizedBox(width: 20),
-                    Radio(
-                      value: 2,
-                      groupValue: _selectedLevel,
-                      onChanged: (value) {
-                        setState(() {
-                          _selectedLevel = value as int;
-                        });
-                      },
-                      activeColor: const Color(0xFF9CCC65),
-                    ),
-                    Text(AppLocalizations.of(context)!.hard),
-                    ],
-                  ),
-                ],
               ),
-            ),
-          );
-        },
-      ),
+            );
+          },
+        ),
     );
   }
 }
